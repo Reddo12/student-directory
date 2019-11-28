b8b5ec8e200c255118221571ca6ec0532dce8de7
Aaron Redden <aaron.redden01@vodafone.com>
2019-11-28 16:26:54 +0000

    challenge 1 add index

--- directory.rb
+++ directory.rb
@@ -1,5 +1,15 @@
 students = [
-  
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},  
+  {name: "Norman Bates", cohort: :november}
 ]
 
 def input_students
@@ -27,8 +37,9 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index() do |student, index|
+    indexplusone = index + 1
+    puts "#{indexplusone}. #{student[:name]} (#{student[:cohort]} cohort)"
  end
 end
 
@@ -36,8 +47,8 @@ def print_footer(students)
  puts "Overall, we have #{students.count} great students"
 end
 #nothing happens untl we call the methods
+
 students = input_students
 print_header
 print(students)
 print_footer(students)
-
